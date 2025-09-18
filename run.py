3fd8fded9855b137b2d84901f630623c7ac5905f
Alumsdesigns <alum.damaris@gmail.com>
2025-09-18 21:04:54 +0100

    chore(deps): added and imported libraries needed

--- run.py
+++ run.py
@@ -1,2 +1,20 @@
-# Your code goes here.
-
+#!/usr/bin/env python3
+"""
+Daily Log Terminal App with Google Sheets Integration
+Author: Damaris Alum
+Date: 14-09-2025
+"""
+import time
+import datetime
+from datetime import timezone
+import os
+import datetime
+import gspread
+import questionary
+from rich.console import Console
+from rich.table import Table
+from gspread_formatting import (
+    format_cell_range, CellFormat, Color, TextFormat, Borders, Border
+)
+from google.oauth2.service_account import Credentials
+from google.auth.exceptions import GoogleAuthError
